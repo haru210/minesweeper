deb6c4dfd1b7faf91b93efb232a974310adb8f36
haru210 <miroku.pr@outlook.jp>
2023-04-22 17:03:20 +0900

    爆弾設置から第一クリックまで

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <random>
+#include <ctime>
 #include <conio.h>
+#include <time.h>
 using namespace std;
 
-int map[10][10] = {0}; //0~8‚ğ”š‚Ü‚·‚Æ‚·‚é
-bool opened[10][10] = {false};
+int map[10][10] = {0}; //0~8‚ğ”š‚Ü‚·‚Æ‚·‚é 9‚ÍŠø -1‚Í”š’e
+bool opened[10][10] = {0};
 
 int selected_x = 0;
 int selected_y = 0;
 
 bool gameend = false;
-void writemap();
+bool firstclick = true;
+
+
+void write_map();
+void place_bomb();
 
 int main()
 {
-    writemap();
+    srand(time(NULL));
+    write_map();
     while(!gameend)
     {
         switch(_getch())
@@ -31,18 +39,29 @@ int main()
             break;
 
             case 'd':
-            if(selected_y != 9) selected_x++;
+            if(selected_x != 9) selected_x++;
             break;
 
+            case ' ':
+            if(map[selected_y][selected_x] != -1)
+            {
+                opened[selected_y][selected_x] = true;
+            }
+            if(firstclick) 
+            {
+                place_bomb();
+                firstclick = false;
+            }
+            break;
             case '\b':
             gameend = true;
             break;
         }
-        writemap();
+        write_map();
     }
 }
 
-void writemap()
+void write_map()
 {
     for(int i = 0; i < 10; i++)
     {
@@ -55,13 +74,41 @@ void writemap()
             else if(!opened[i][j])
             {
                 cout << "¡" << ' ';
-            }
+            } 
             else
             {
-                cout << map[i][j];
+                switch(map[i][j])
+                {
+                    case -1:
+                    cout << 'X' << ' ';
+                    break;
+                    default:
+                    cout << "E";
+                    break;
+                }
             }
         }
         cout << '\n';
     }
     cout << '\n';
+}
+
+void place_bomb()
+{
+    int bomb_x, bomb_y;
+    int i = 0;
+    for(int i = 0;i < 10; i++)
+    {
+        bomb_x = rand() % 10;
+        bomb_y = rand() % 10;
+        if(map[bomb_y][bomb_x] == -1 || opened[bomb_y][bomb_x] == true)
+        {
+            i--;
+        }
+        else
+        {
+            map[bomb_y][bomb_x] = -1;
+        }
+    }
+    
 }
